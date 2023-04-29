8a1fd7f264b6f147592c797de54f34935dbd8cd9
Christian Smith <christiansmith@Christians-MBP.attlocal.net>
2023-04-29 11:08:22 -0400

    Worked with tutor Bobbi to get file structure set and setup certain files + setup user model

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,1 +1,18 @@
 const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+   try {
+    // Query Database for post
+    res.render('homepage', {
+        // Hand off posts
+        logged_in: req.session.logged_in 
+    })
+   } catch (err) {
+    res.status(500).json(err);
+   }
+})
+
+
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,8 @@
 const router = require('express').Router();
+const homeRoutes = require('./homeRoute');
+const api = require('./api');
+
+router.use('/', homeRoutes);
+router.use('/api', api);
+
+module.exports = router;
